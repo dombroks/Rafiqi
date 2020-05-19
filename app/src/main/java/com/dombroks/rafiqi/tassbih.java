00f4c0bbb6c9b8d414f0f2e76e41f20696d40de9
dombroks <tinoutech@gmail.com>
2020-05-19 19:18:29 +0100

    intial commit

--- app/src/main/java/com/dombroks/rafiqi/tassbih.java
+++ app/src/main/java/com/dombroks/rafiqi/tassbih.java
@@ -3,12 +3,40 @@ package com.dombroks.rafiqi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class tassbih extends AppCompatActivity {
+    private LinearLayout plusLinearLayout;
+    private int index;
+    private TextView counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tassbih);
+        plusLinearLayout = findViewById(R.id.linearLayout);
+        counter = findViewById(R.id.index);
+        index = 0;
+        counter.setText(String.valueOf(index));
+
+
+        plusLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index++;
+                counter.setText(String.valueOf(index));
+                if (index == 34) {
+                    index = 0;
+                    counter.setText(String.valueOf(index));
+
+                }
+            }
+        });
+
+
+
     }
+
 }
