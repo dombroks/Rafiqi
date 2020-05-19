cde3191be7c818c58c0578dcf6d81ad259b242ff
dombroks <tinoutech@gmail.com>
2020-05-19 20:23:18 +0100

    intial commit

---
+++ app/src/main/java/com/dombroks/rafiqi/chahada.java
@@ -0,0 +1,14 @@
+package com.dombroks.rafiqi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chahada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chahada);
+    }
+}
